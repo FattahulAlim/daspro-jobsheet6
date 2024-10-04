a041625f033ecf888bfa809862f94aaf74ef25f3
FattahulAlim <fathalim1512@gmail.com>
2024-10-04 13:06:07 +0700

    tahun kabisat change

--- pemilihan2percobaan116.java
+++ pemilihan2percobaan116.java
@@ -10,8 +10,12 @@ public class pemilihan2percobaan116 {
         tahun = input.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0) {
                 System.out.println("Tahun kabisat");
+            } else {
+                System.out.println("Bukan tahun kabisat");
+            }
+            
         } else
             System.out.println("Bukan tahun kabisat");
     }
